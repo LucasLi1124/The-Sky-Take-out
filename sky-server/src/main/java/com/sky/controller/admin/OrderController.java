0da40f5a1f4809fb2ca8948eb817113c27cf0270
LucasLi1124 <136773489+LucasLi1124@users.noreply.github.com>
2025-06-11 11:43:06 +0800

    fix(controller): remove invalid apache commons imports

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -13,7 +13,6 @@ import com.sky.vo.OrderVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.Put;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
